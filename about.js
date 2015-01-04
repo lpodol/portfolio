401f245e63a087d237574a63a1cbd682229bf87e
Lauren Podolnick <lauren@Adam-Podolnicks-MacBook-Pro.local>
2015-01-04 16:55:54 -0500

    first commit

---
+++ about.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(function(req,res){
+
+  if (request.url === "./index.html"){
+    fs.readFile("index.html", function(err,data){
+      response.end(data);
+    });
+  }else if (request.url === "./style.css"){
+    fs.readFile("stylesheet.css", function(err,data) {
+      response.end(data);
+    });
+  }else if (request.url === "./about.html"){
+    fs.readFile("about.html", function(err,data) {
+      response.end(data);
+    });
+  }else if (request.url === "./portfolio.html"){
+    fs.readFile("contact.html", function(err,data) {
+      response.end(data);
+    });
+  }else if (request.url === "./contact.html") {
+    fs.readFile("contact.html", function(err,data) {
+      response.end(data);
+    });
+  }else (request.url === ""){
+      fs.readFile("index.html", function(err,data) {
+        response.end(data);
+    });
+  }
+});
+
+server.listen(80);
