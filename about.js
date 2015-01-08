c2131b52efed99f94c91a4a881302ec405f3e83d
Lauren Podolnick <lauren@Adam-Podolnicks-MacBook-Pro.local>
2015-01-08 15:00:03 -0500

    sixth commit

--- about.js
+++ about.js
@@ -1,36 +1,35 @@
 var http = require("http");
 var fs = require("fs");
-var server = http.createServer(function(req,res){
+var server = http.createServer(function(request,response){
 
   if (request.url === "./index.html"){
     fs.readFile("index.html", function(err,data){
       response.end(data);
     });
   }else if (request.url === "./style.css"){
-    fs.readFile("stylesheet.css", function(err,data) {
+    fs.readFile("style.css", function(err,data){
       response.end(data);
     });
   }else if (request.url === "./about.html"){
-    fs.readFile("about.html", function(err,data) {
+    fs.readFile("about.html", function(err,data){
       response.end(data);
     });
   }else if (request.url === "./portfolio.html"){
-    fs.readFile("contact.html", function(err,data) {
+    fs.readFile("portfolio.html", function(err,data){
       response.end(data);
     });
-  }else if (request.url === "./contact.html") {
-    fs.readFile("contact.html", function(err,data) {
+  }else if (request.url === "./contact.html"){
+    fs.readFile("contact.html", function(err,data){
       response.end(data);
     });
-  }else if (request.url === "./chatroom_index.html") {
+  }else if (request.url === "./chatroom_index.html"){
     fs.readFile("chatroom_index.html", function(err,data) {
       response.end(data);
-    })
-  }else if (request.url === "./contact.html") {
-    fs.readFile("contact.html", function(err,data) {
+    });
+  }else{
+    fs.readFile("index.html", function(err,data){
       response.end(data);
-    });;
-  };
+    });
+  }
 });
-
 server.listen(80);
